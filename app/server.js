14bb96d3753db2f4d5195cd2393919e14901dccb
PecataP <pepo_da@abv.bg>
2025-11-04 19:46:28 +0100

    test2

---
+++ app/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('Hello from secure CI/CD with SLSA + Cosign!\n');
+});
+
+server.listen(port, () => console.log(`Listening on ${port}`));
