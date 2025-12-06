94a5e27b034998b89b8cf40459163c54e081f293
PecataP <pepo_da@abv.bg>
2025-12-06 16:28:03 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""
-Simple Python web application for SLSA security demonstration
-"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
@@ -12,7 +9,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'Hello from secure CI/CD with SLSA!\n')
+        self.wfile.write(b'Hello from my SLSA imroved pipeline!\n')
 
     def log_message(self, format, *args):
         # Print logs to stdout for container logging
@@ -26,4 +23,5 @@ def run_server():
 
 if __name__ == '__main__':
     run_server()
+
 
